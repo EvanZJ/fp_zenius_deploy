1cbd52b35333a8d461993357519f952ec0e192ac
unknown <januar.evan2012@gmail.com>
2022-12-09 21:40:57 +0700

    well this is new

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ for i in val_col:
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    url = 'https://drive.google.com/uc?id=1n_CObRNFjYx360I6D5r0lcUFZiU5JkCG'
+    url = 'https://drive.google.com/uc?id=1R9hel7MoJjW2U-9RpRSz1jHTQZO-_mnz'
     output = 'model.pkl'
     gdown.download(url, output, quiet=True)
     model = joblib.load('model.pkl')
